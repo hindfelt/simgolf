776c07d7a7dccf94fc8cbd73339a491e2e98cb55
mathin <hindfelt@gmail.com>
2026-09-11 12:33:49 +0200

    Expose new game and remove production art study entry

--- simgolf-reborn/scene/src/entry.js
+++ simgolf-reborn/scene/src/entry.js
@@ -1,4 +1,4 @@
-if (new URLSearchParams(location.search).get("mode") === "art") {
+if (import.meta.env.DEV && new URLSearchParams(location.search).get("mode") === "art") {
   await import("./main.js");
 } else {
   await import("./play.js");
